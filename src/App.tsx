c2c70b0dc032ba9f73fd065b70b568d54f72c052
FokinAleksandr <shwayze@yandex.ru>
2022-06-21 04:00:28 +0300

    feat: add basic fetching capabilities (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,56 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+import { useArticles } from './useArticles';
 
 export function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <Text>text</Text>
+        <Screen />
       </SafeAreaView>
     </SafeAreaProvider>
+  );
+}
+
+type SectionsType =
+  'arts'
+  | 'automobiles'
+  | 'books'
+  | 'business'
+  | 'fashion'
+  | 'food'
+  | 'health'
+  | 'home'
+  | 'insider'
+  | 'magazine'
+  | 'movies'
+  | 'nyregion'
+  | 'obituaries'
+  | 'opinion'
+  | 'politics'
+  | 'realestate'
+  | 'science'
+  | 'sports'
+  | 'sundayreview'
+  | 'technology'
+  | 'theater'
+  | 't-magazine'
+  | 'travel'
+  | 'upshot'
+  | 'us'
+  | 'world';
+
+function Screen() {
+  const [section, setSection] = React.useState<SectionsType>('home');
+  const [keywords, setKeywords] = React.useState('');
+  const [location, setLocation] = React.useState('');
+  const { data, status } = useArticles(section, keywords, location);
+
+  return (
+    <Text>
+      {JSON.stringify(data, null, 2)}
+    </Text>
   );
 }
